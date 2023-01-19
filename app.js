ac7846bb057457e13905f21d78b5f8a45fcd8dcb
HydraRetro <119288194+HydraRetro@users.noreply.github.com>
2023-01-19 00:40:39 -0800

    Added JS file with basic operators

---
+++ app.js
@@ -0,0 +1,22 @@
+
+// Math Functions
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+// Operator
+function operate(operator, x, y) {
+
+}
