1ddabc704ebcd915b93890df5d482d581048c0d9
HydraRetro <119288194+HydraRetro@users.noreply.github.com>
2023-01-20 17:39:54 -0800

    Added calcualtor code

--- app.js
+++ app.js
@@ -1,22 +1,93 @@
+let operator = '';
+let previousValue = '';
+let currentValue = '';
 
-// Math Functions
-function add(x, y) {
-    return x + y;
+document.addEventListener('DOMContentLoaded', function () {
+    let clear = document.querySelector('#clear-btn');
+    let equal = document.querySelector('.equal');
+    let decimal = document.querySelector('.decimal');
+
+    let numbers = document.querySelectorAll('.number');
+    let operators = document.querySelectorAll('.operator');
+
+    let previousScreen = document.querySelector('.previous');
+    let currentScreen = document.querySelector('.current');
+
+    numbers.forEach((number) => number.addEventListener('click', function (e) {
+        handleNumber(e.target.textContent)
+        currentScreen.textContent = currentValue;
+    }))
+
+    operators.forEach((op) => op.addEventListener('click', function (e) {
+        handleOperator(e.target.textContent)
+        previousScreen.textContent = previousValue + " " + operator;
+        currentScreen.textContent = currentValue;
+    }))
+
+    clear.addEventListener('click', function () {
+        previousValue = '';
+        currentValue = '';
+        operator = '';
+        previousScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    })
+
+    equal.addEventListener('click', function () {
+        if (currentValue != '' && previousValue != '') {
+            calculate();
+            previousScreen.textContent = '';
+            if (previousValue.length <= 5) {
+                currentScreen.textContent = previousValue;
+            } else {
+                currentScreen.textContent = previousValue.slice(0, 5) + "...";
+            }
+        }
+    })
+
+    decimal.addEventListener('click', function () {
+        addDecimal();
+    })
+});
+
+function handleNumber(num) {
+    if (currentValue.length <= 5) {
+        currentValue += num;
+    }
 }
 
-function subtract(x, y) {
-    return x - y;
+function handleOperator(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
 }
 
-function multiply(x, y) {
-    return x * y;
+function calculate() {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if (operator === '+') {
+        previousValue += currentValue;
+    } else if (operator === '-') {
+        previousValue -= currentValue;
+    } else if (operator === 'x') {
+        previousValue *= currentValue;
+    } else {
+        previousValue /= currentValue;
+    }
+
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
 }
 
-function divide(x, y) {
-    return x / y;
+function roundNumber(num) {
+    return Math.round(num * 1000) / 1000;
 }
 
-// Operator
-function operate(operator, x, y) {
-
+function addDecimal(params) {
+    if (!currentValue.includes('.')) {
+        currentValue += '.';
+    }
 }
+
+
